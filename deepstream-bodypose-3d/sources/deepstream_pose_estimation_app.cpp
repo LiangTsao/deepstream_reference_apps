f8afbac85d8894cfcbc8e8fe0ae278de759dd5be
LiangTsao <LiangTsao@qq.com>
2022-06-26 16:31:27 +0800

    Update deepstream_pose_estimation_app.cpp

--- deepstream-bodypose-3d/sources/deepstream_pose_estimation_app.cpp
+++ deepstream-bodypose-3d/sources/deepstream_pose_estimation_app.cpp
@@ -1133,7 +1133,7 @@ sgie_src_pad_buffer_probe(GstPad *pad, GstPadProbeInfo *info,
         fprintf(_pose_file, ", ");
       }
     }
-    g_print ("\n\n\n\n\n\nEND batch_id=%d frame_num=%d num_obj_meta=%d i=%d \n\n\n\n\n\n",
+    g_print ("\n\n\n\n\n\nEND batch_id=%d frame_num=%d num_obj_meta=%d \n\n\n\n\n\n",
                      frame_meta->batch_id,
                      frame_meta->frame_num,
                      frame_meta->num_obj_meta);
